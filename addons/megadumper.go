a5489e1f5fe293d9af9faacd39a3350bf91050c1
Robert Terhaar <rterhaar@atlanticdynamic.com>
2024-02-05 16:46:01 -0500

    output log as json

--- addons/megadumper.go
+++ addons/megadumper.go
@@ -2,6 +2,7 @@ package addons
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -36,6 +37,22 @@ type MegaDumper struct {
 	logLevel            LogLevel
 }
 
+type requestLogDump struct {
+	RequestHeaders  string `json:"request_headers,omitempty"`
+	RequestBody     string `json:"request_body,omitempty"`
+	ResponseHeaders string `json:"response_headers,omitempty"`
+	ResponseBody    string `json:"response_body,omitempty"`
+}
+
+// toJSONBytes converts the requestLogDump struct to a byte array, omitting fields that are empty
+func (d *requestLogDump) toJSONBytes() ([]byte, error) {
+	j, err := json.MarshalIndent(d, "", "  ")
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal requestLogDump to JSON: %w", err)
+	}
+	return j, nil
+}
+
 func (d *MegaDumper) Requestheaders(f *proxy.Flow) {
 	go func() {
 		<-f.Done()
@@ -44,7 +61,11 @@ func (d *MegaDumper) Requestheaders(f *proxy.Flow) {
 }
 
 func (d MegaDumper) Write(f *proxy.Flow) error {
-	bytesToWrite, err := d.prepDumpBytes(f)
+	logDump, err := d.prepJSONobj(f)
+	if err != nil {
+		return err
+	}
+	bytesToWrite, err := logDump.toJSONBytes()
 	if err != nil {
 		return err
 	}
@@ -68,14 +89,79 @@ func (d MegaDumper) Write(f *proxy.Flow) error {
 	return d.diskWriter(bytesToWrite)
 }
 
-func (d MegaDumper) diskWriter(buf *bytes.Buffer) error {
+func (d MegaDumper) diskWriter(bytes []byte) error {
 	if d.singleLogFileTarget == nil {
 		return fmt.Errorf("internal error: singleLogFileTarget is not set")
 	}
 
 	log.Debugf("Writing to log file: %v", d.logFilename)
-	_, err := d.singleLogFileTarget.Write(buf.Bytes())
+	_, err := d.singleLogFileTarget.Write(bytes)
 	return err
+}
+
+// prepJSONobj is a blocking call, run by .Write after <-f.Done() (alternative to prepDumpBytes)
+func (d *MegaDumper) prepJSONobj(f *proxy.Flow) (*requestLogDump, error) {
+	req := &requestLogDump{}
+
+	// request headers
+	switch d.logLevel {
+	case WRITE_REQ_HEADERS_ONLY, WRITE_REQ_HEADERS_ALSO_RESP_HEADERS_AND_RESP_BODY:
+		log.Debug("Dumping request headers")
+		if f.Request != nil {
+			buf := new(bytes.Buffer)
+			if err := f.Request.Header.WriteSubset(buf, nil); err != nil {
+				log.Error(err)
+			} else {
+				req.RequestHeaders = buf.String()
+			}
+		} else {
+			log.Error("request is nil, unable to write request headers")
+		}
+	}
+
+	// request body
+	switch d.logLevel {
+	case WRITE_REQ_BODY_AND_RESP_BODY, WRITE_REQ_HEADERS_AND_REQ_BODY_ALSO_RESP_HEADERS_AND_RESP_BODY:
+		log.Debug("Dumping request body")
+		if f.Request != nil {
+			if f.Request != nil && len(f.Request.Body) > 0 && canPrint(f.Request.Body) {
+				req.RequestBody = string(f.Request.Body)
+			}
+		} else {
+			log.Error("request is nil, unable to write request headers")
+		}
+	}
+
+	// response headers
+	switch d.logLevel {
+	case WRITE_REQ_HEADERS_ALSO_RESP_HEADERS_AND_RESP_BODY, WRITE_REQ_HEADERS_AND_REQ_BODY_ALSO_RESP_HEADERS_AND_RESP_BODY:
+		log.Debug("Dumping response headers")
+		if f.Response != nil {
+			buf := new(bytes.Buffer)
+			err := f.Response.Header.WriteSubset(buf, nil) // writing response headers
+			if err != nil {
+				// continue here, if unable to store the full response
+				log.Error(err)
+			} else {
+				req.ResponseHeaders = buf.String()
+			}
+
+		}
+	}
+
+	// response body
+	switch d.logLevel {
+	case WRITE_REQ_BODY_AND_RESP_BODY, WRITE_REQ_HEADERS_ALSO_RESP_HEADERS_AND_RESP_BODY, WRITE_REQ_HEADERS_AND_REQ_BODY_ALSO_RESP_HEADERS_AND_RESP_BODY:
+		log.Debug("Dumping response body")
+		if f.Response.Body != nil && len(f.Response.Body) > 0 && f.Response.IsTextContentType() {
+			body, err := f.Response.DecodedBody()
+			if err == nil && body != nil && len(body) > 0 {
+				req.ResponseBody = string(body)
+			}
+		}
+	}
+
+	return req, nil
 }
 
 // prepDumpBytes is a blocking call, run by .Write after <-f.Done()
@@ -105,22 +191,6 @@ func (d *MegaDumper) prepDumpBytes(f *proxy.Flow) (*bytes.Buffer, error) {
 		log.Debug("Dumping request body")
 		if f.Request != nil {
 			if f.Request != nil && len(f.Request.Body) > 0 && canPrint(f.Request.Body) {
-				/*
-					bod, err := f.Request.Raw().GetBody()
-					if err != nil {
-						log.Error(err)
-						break
-					}
-
-					_, err = io.ReadAll(bod)
-					if err != nil {
-						log.Error(err)
-						break
-					}
-
-					// buf.Write(bodyBytes)
-					//
-				*/
 				_, err := buf.Write(f.Request.Body)
 				if err != nil {
 					log.Error(err)
