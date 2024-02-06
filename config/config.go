f4be3be68af46217c433f355aa91f55d44142ac2
Robert Terhaar <rterhaar@atlanticdynamic.com>
2024-02-06 15:34:23 -0500

    move the log config options to methods on the config struct, add tests

--- config/config.go
+++ config/config.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	log "github.com/sirupsen/logrus"
+)
+
 // Config objects configure the proxy proxy.
 type Config struct {
 	Listen    string // Local address the proxy should listen on
@@ -11,6 +15,34 @@ type Config struct {
 	WriteJsonFormatLogs   bool // if true, write logs in JSON format
 	Verbose               bool // if true, print runtime activity to stdout
 	Debug                 bool // if true, print debug information to stderr
+	logLevelHasBeenSet    bool // internal flag to track if the log level has been set
+}
+
+// SetLoggerLevel sets the logrus level based on verbose/debug values in the config object
+func (cfg *Config) SetLoggerLevel() {
+	if cfg.Debug {
+		log.SetLevel(log.DebugLevel)
+		// enable this for full code tracing output
+		// log.SetReportCaller(true)
+	} else if cfg.Verbose {
+		log.SetLevel(log.InfoLevel)
+	} else {
+		log.SetLevel(log.WarnLevel)
+	}
+	log.Info("Logger level set to: ", log.GetLevel())
+	cfg.logLevelHasBeenSet = true
+}
+
+// GetDebugLevel returns 1 if the log level is debug, 0 otherwise, for use in the proxy package
+func (cfg *Config) GetDebugLevel() int {
+	if !cfg.logLevelHasBeenSet {
+		cfg.SetLoggerLevel()
+	}
+
+	if log.GetLevel() >= log.DebugLevel {
+		return 1
+	}
+	return 0
 }
 
 func GetDefaultConfig() *Config {
