cf2b491a16a2784ca38271080e075f1a63a3fa70
Robert Terhaar <rterhaar@atlanticdynamic.com>
2024-02-23 14:59:06 -0500

    add test for the toDir writer

---
+++ addons/megadumper/writers/toDir_test.go
@@ -0,0 +1,36 @@
+package writers_test
+
+import (
+	"os"
+	"testing"
+
+	md "github.com/robbyt/llm_proxy/addons/megadumper"
+	"github.com/robbyt/llm_proxy/addons/megadumper/writers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToDir_Write(t *testing.T) {
+	// Create a temporary directory for testing
+	tempDir, err := os.MkdirTemp("", "test")
+	assert.NoError(t, err)
+	defer os.RemoveAll(tempDir) // clean up
+
+	// Create a new ToDir instance
+	toDir, err := writers.NewToDir(tempDir, md.Format_PLAINTEXT)
+	assert.NoError(t, err)
+
+	// Write some data
+	data := []byte("test data")
+	n, err := toDir.Write("test", data)
+	assert.NoError(t, err)
+	assert.Equal(t, len(data), n)
+
+	// Check that the file was created and contains the correct data
+	files, err := os.ReadDir(tempDir)
+	assert.NoError(t, err)
+	assert.Len(t, files, 1)
+
+	fileData, err := os.ReadFile(tempDir + "/" + files[0].Name())
+	assert.NoError(t, err)
+	assert.Equal(t, data, fileData)
+}
