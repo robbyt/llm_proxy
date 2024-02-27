94dfe2c22c27d50ddcb0ebbc3323abcc56b809f2
Robert Terhaar <rterhaar@atlanticdynamic.com>
2024-02-26 19:32:54 -0500

    run the stdout loggers each in a goroutine, for performance (and change log level to info)

--- addons/stdoutlogger.go
+++ addons/stdoutlogger.go
@@ -14,39 +14,47 @@ type StdOutLogger struct {
 }
 
 func (addon *StdOutLogger) ClientConnected(client *px.ClientConn) {
-	log.DebugFn(func() []interface{} {
-		return []interface{}{fmt.Sprintf("client connect: %v", client.Conn.RemoteAddr())}
-	})
+	go func() {
+		log.InfoFn(func() []interface{} {
+			return []interface{}{fmt.Sprintf("client connect: %v", client.Conn.RemoteAddr())}
+		})
+	}()
 }
 
 func (addon *StdOutLogger) ClientDisconnected(client *px.ClientConn) {
-	log.DebugFn(func() []interface{} {
-		return []interface{}{fmt.Sprintf("client disconnect: %v", client.Conn.RemoteAddr())}
-	})
+	go func() {
+		log.InfoFn(func() []interface{} {
+			return []interface{}{fmt.Sprintf("client disconnect: %v", client.Conn.RemoteAddr())}
+		})
+	}()
 }
 
 func (addon *StdOutLogger) ServerConnected(connCtx *px.ConnContext) {
-	log.DebugFn(func() []interface{} {
-		return []interface{}{
-			fmt.Sprintf("server connect: %v (%v->%v)",
-				connCtx.ServerConn.Address,
-				connCtx.ServerConn.Conn.LocalAddr(),
-				connCtx.ServerConn.Conn.RemoteAddr(),
-			),
-		}
-	})
+	go func() {
+		log.InfoFn(func() []interface{} {
+			return []interface{}{
+				fmt.Sprintf("server connect: %v (%v->%v)",
+					connCtx.ServerConn.Address,
+					connCtx.ServerConn.Conn.LocalAddr(),
+					connCtx.ServerConn.Conn.RemoteAddr(),
+				),
+			}
+		})
+	}()
 }
 
 func (addon *StdOutLogger) ServerDisconnected(connCtx *px.ConnContext) {
-	log.DebugFn(func() []interface{} {
-		return []interface{}{
-			fmt.Sprintf("server disconnect: %v (%v->%v)",
-				connCtx.ServerConn.Address,
-				connCtx.ServerConn.Conn.LocalAddr(),
-				connCtx.ServerConn.Conn.RemoteAddr(),
-			),
-		}
-	})
+	go func() {
+		log.InfoFn(func() []interface{} {
+			return []interface{}{
+				fmt.Sprintf("server disconnect: %v (%v->%v)",
+					connCtx.ServerConn.Address,
+					connCtx.ServerConn.Conn.LocalAddr(),
+					connCtx.ServerConn.Conn.RemoteAddr(),
+				),
+			}
+		})
+	}()
 }
 
 func NewStdOutLogger() *StdOutLogger {
