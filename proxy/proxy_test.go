ea16db049afc49583d6cfd79ef814a81d83bd199
Robert Terhaar <rterhaar@atlanticdynamic.com>
2024-03-23 22:44:40 -0400

    listen on a random port

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -18,11 +19,23 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	testServerListenAddr = "localhost:8182"
-)
+// randomly finds an available port to bind to
+func getFreePort() (string, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return "", err
+	}
 
-func webServer(hitCounter *atomic.Int32) (*http.Server, func()) {
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return "", err
+	}
+	defer l.Close()
+	port := l.Addr().(*net.TCPAddr).Port
+	return fmt.Sprintf("localhost:%d", port), nil
+}
+
+func webServer(hitCounter *atomic.Int32, listenAddr string) (*http.Server, func()) {
 	if hitCounter == nil {
 		panic("hitCounter must be non-nil")
 	}
@@ -37,7 +50,7 @@ func webServer(hitCounter *atomic.Int32) (*http.Server, func()) {
 	})
 
 	srv := &http.Server{
-		Addr:    testServerListenAddr,
+		Addr:    listenAddr,
 		Handler: mux,
 	}
 
@@ -71,10 +84,17 @@ func httpClient(proxyAddr string) (*http.Client, error) {
 }
 
 func TestProxySimple(t *testing.T) {
+	proxyPort, err := getFreePort()
+	require.NoError(t, err)
+
+	testServerPort, err := getFreePort()
+	require.NoError(t, err)
+
 	// Create a simple proxy config
 	cfg := config.NewDefaultConfig()
 	cfg.CertDir = t.TempDir()
 	cfg.NoHttpUpgrader = true // disable TLS because our test server doesn't support it
+	cfg.Listen = proxyPort
 	cfg.AppMode = config.SimpleMode
 
 	// create a proxy with the test config
@@ -95,18 +115,18 @@ func TestProxySimple(t *testing.T) {
 	// Start a basic web server on another port
 	hitCounter := new(atomic.Int32)
 
-	srv, srvShutdown := webServer(hitCounter)
+	srv, srvShutdown := webServer(hitCounter, testServerPort)
 	require.NotNil(t, srv)
 	require.NotNil(t, srvShutdown)
 
 	// Create a client that will use the proxy
-	client, err := httpClient("http://" + cfg.Listen)
+	client, err := httpClient("http://" + proxyPort)
 	require.NoError(t, err)
 
 	t.Run("TestSimpleProxy", func(t *testing.T) {
 		hitCounter.Store(0) // reset the counter
 		// make a request using that client, through the proxy
-		resp, err := client.Post("http://"+testServerListenAddr, "text/plain", strings.NewReader("hello"))
+		resp, err := client.Post("http://"+testServerPort, "text/plain", strings.NewReader("hello"))
 		require.NoError(t, err)
 		assert.Equal(t, 200, resp.StatusCode)
 
@@ -120,7 +140,7 @@ func TestProxySimple(t *testing.T) {
 	t.Run("TestSimpleProxy2", func(t *testing.T) {
 		hitCounter.Store(5) // reset the counter
 		// make another request using that client, through the proxy
-		resp, err := client.Post("http://"+testServerListenAddr, "text/plain", strings.NewReader("hello"))
+		resp, err := client.Post("http://"+testServerPort, "text/plain", strings.NewReader("hello"))
 		require.NoError(t, err)
 		assert.Equal(t, 200, resp.StatusCode)
 
@@ -139,12 +159,19 @@ func TestProxySimple(t *testing.T) {
 }
 
 func TestProxyCache(t *testing.T) {
+	proxyPort, err := getFreePort()
+	require.NoError(t, err)
+
+	testServerPort, err := getFreePort()
+	require.NoError(t, err)
+
 	tmpDir := t.TempDir()
 	// Create a simple proxy config
 	cfg := config.NewDefaultConfig()
 	cfg.CertDir = tmpDir + "/certs"
 	cfg.Cache.Dir = tmpDir + "/cache"
 	cfg.NoHttpUpgrader = true // disable TLS because our test server doesn't support it
+	cfg.Listen = proxyPort
 	cfg.AppMode = config.CacheMode
 
 	// create a proxy with the test config
@@ -164,7 +191,7 @@ func TestProxyCache(t *testing.T) {
 
 	// Start a basic web server on another port
 	hitCounter := new(atomic.Int32)
-	srv, srvShutdown := webServer(hitCounter)
+	srv, srvShutdown := webServer(hitCounter, testServerPort)
 	require.NotNil(t, srv)
 	require.NotNil(t, srvShutdown)
 
@@ -175,9 +202,9 @@ func TestProxyCache(t *testing.T) {
 	t.Run("TestCacheMiss", func(t *testing.T) {
 		hitCounter.Store(0) // reset the counter
 		// make a request using that client, through the proxy
-		resp, err := client.Post("http://"+testServerListenAddr, "text/plain", strings.NewReader("hello"))
+		resp, err := client.Post("http://"+testServerPort, "text/plain", strings.NewReader("hello"))
 		require.NoError(t, err)
-		assert.Equal(t, 200, resp.StatusCode)
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
 
 		// check the response body from req1
 		body, err := io.ReadAll(resp.Body)
@@ -190,7 +217,7 @@ func TestProxyCache(t *testing.T) {
 	t.Run("TestCacheHit", func(t *testing.T) {
 		hitCounter.Store(5) // reset the counter
 		// make another request using that client, through the proxy
-		resp, err := client.Post("http://"+testServerListenAddr, "text/plain", strings.NewReader("hello"))
+		resp, err := client.Post("http://"+testServerPort, "text/plain", strings.NewReader("hello"))
 		require.NoError(t, err)
 		assert.Equal(t, 200, resp.StatusCode)
 
